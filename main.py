c8e883dc1e8f1697ff83611fa4399e4985087f2f
glert1 <117526803+glert1@users.noreply.github.com>
2024-02-01 19:49:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import qrcode
+
+# The URL you want to encode in the QR code
+url = "https://www/"
+
+# Generate the QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image from the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image to a file
+img.save("qrcode.png")
